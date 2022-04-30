4db5b6ef44fb09429f661f87c79f2fed2b18210b
Ruth Evans <ruthevans@outlook.com>
2022-04-30 17:55:25 +0100

    Added new button with text prompt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QInputDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,22 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_newButton_clicked()
+{
+    bool ok = false;
+    QString text = QInputDialog::getText(/*parent*/this,
+                                         /*dialog title*/tr("New CirrusList"),
+                                         /*label text*/tr("CirrusList subject:"),
+                                         /*text display mode*/QLineEdit::Normal,
+                                         /*sample text*/tr(""), &ok);
+
+    if (ok && !text.isEmpty())
+    {
+         //open new
+    }
+
 }
 
